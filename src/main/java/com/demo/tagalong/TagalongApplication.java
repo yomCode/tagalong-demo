638faebc0c3d69ed4c316a51f9c191fdf7da5a20
Abayomi Mustapha <mustaphaabayomi71@gmail.com>
2023-03-29 08:26:39 +0100

    Project init

---
+++ src/main/java/com/demo/tagalong/TagalongApplication.java
@@ -0,0 +1,13 @@
+package com.demo.tagalong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagalongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagalongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/tagalong/TagalongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.tagalong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagalongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
